6f8bf29dbd6ffd4353310a9914660f84330c28ef
Ayu Wardatul <ayuwardatuwwl@gmail.com>
2026-02-20 13:29:52 +0700

    2.1 Percobaan 1

---
+++ P2/Mahasiswa04.java
@@ -0,0 +1,32 @@
+package P2;
+
+public class Mahasiswa04 {
+ String nama04;
+ String nim04;
+ String kelas04;
+ double ipk04;
+
+ void tampilkanInformais (){
+    System.out.println("Nama : "+ nama04);
+    System.out.println("Nim : "+nim04);
+    System.out.println("IPK : "+ipk04);
+    System.out.println("Kelas : "+kelas04);
+ }
+ void UbahKelas (String kelasbaru) {
+   kelas04 = kelasbaru;
+ }
+ void updateIpk (double ipkBaru){
+   ipk04 = ipkBaru;
+ }
+ String nilaiKinerja (){
+   if (ipk04 >= 3.5) {
+      return "Kinerja sangat baik";
+   } else if (ipk04 >= 3.0) {
+      return "Kinerja baik";
+   }else if (ipk04 >= 2.0) {
+      return "Kinerja cukup";
+   }else {
+      return "Kinerja kurang";
+   }
+ }
+}
