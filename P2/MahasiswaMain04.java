a091b77ec822a0bedb758b6a6bbbe477d2fb0040
Ayu Wardatul <ayuwardatuwwl@gmail.com>
2026-02-20 15:13:38 +0700

    2.3 Pertanyaan No.5

--- P2/MahasiswaMain04.java
+++ P2/MahasiswaMain04.java
@@ -13,6 +13,12 @@ public class MahasiswaMain04 {
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformais();
 
-       
+        Mahasiswa04 mhs2 = new Mahasiswa04("Annisa nabila","2141720160", 3.25 , "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformais();
+
+        Mahasiswa04 mhs3 = new Mahasiswa04("Ayu Wardatul Widad","254107020171", 3.95,"TI 1E" );
+        mhs3.updateIpk(3.98);
+        mhs3.tampilkanInformais();
     }
 }
