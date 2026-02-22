9c6d5be43019829571c1e9c904d0b0eecf79cbee
Ayu Wardatul <ayuwardatuwwl@gmail.com>
2026-02-22 10:17:47 +0700

    2.4 Percobaan 4 No.1

--- P2/MataKuliahMain04.java
+++ P2/MataKuliahMain04.java
@@ -1,20 +1,33 @@
 package P2;
 public class MataKuliahMain04 {
     public static void main(String[] args) {
+        
         MataKuliah04 mhs1 = new MataKuliah04();
         mhs1.KodeMK04 = "ASD";
         mhs1.Nama04 = "Ayu";
         mhs1.Sks04 = 10;
-        mhs1.JumlahJam04 = 17;
-
-        mhs1.tampilkanInformasi();
-        mhs1.UbahSks(12);
-        mhs1.tambahJam(2);
-        mhs1.kurangiJam(3);
+        mhs1.JumlahJam04 = 17; 
 
         MataKuliah04 mhs2 = new MataKuliah04("AWD","Wardatul",15,25);
-        mhs2.kurangiJam(4);
-        mhs2.tampilkanInformasi();
-    }
 
+        System.out.println("=== DATA LAMA ===");
+        mhs1.tampilkanInformasi();
+        System.out.println();
+        mhs2.tampilkanInformasi();
+        
+        System.out.println();
+        System.out.println("=== DATA TERBARU ===");
+        System.out.println();
+        
+        mhs1.UbahSks(12);
+        mhs1.tambahJam(2);
+        
+        mhs2.kurangiJam(4);
+
+        mhs1.tampilkanInformasi();
+        System.out.println();
+        mhs2.tampilkanInformasi();
+        
+    }
+    
 }
