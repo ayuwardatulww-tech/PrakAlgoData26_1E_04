186a99d2aa195afb975a9ada9fac24c98e6a418d
Ayu Wardatul <ayuwardatuwwl@gmail.com>
2026-03-12 10:13:25 +0700

    5.3 Pertanyaan No. 3

--- P5/pangkat04.java
+++ P5/pangkat04.java
@@ -6,10 +6,10 @@ public class pangkat04 {
         nilai = n;
         pangkat = p;
     }
-    int pangkatBF(int a, int n){
+    int pangkatBF(){
         int hasil = 1;
-        for (int i = 0; i < n; i++) {
-            hasil = hasil*a;
+        for (int i = 0; i < pangkat; i++) {
+            hasil = hasil*nilai;
         }
         return hasil;
     }
