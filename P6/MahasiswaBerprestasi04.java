0fde606343db52c867e89abbf4efbfb574e52150
Ayu Wardatul <ayuwardatuwwl@gmail.com>
2026-04-05 16:30:52 +0700

    5.4 Praktikum 3

--- P6/MahasiswaBerprestasi04.java
+++ P6/MahasiswaBerprestasi04.java
@@ -42,4 +42,14 @@ public class MahasiswaBerprestasi04 {
             listMhs[i] = tmp;
         }
     }
+    void insertionSort (){
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa04 temp = listMhs[i];
+            int j = i;
+            while (j>0 && listMhs[j-1].ipk04>temp.ipk04) {
+                listMhs[j] = listMhs[j-1];
+            }
+            listMhs[j] = temp;
+        }
+    }
 }
--- P6/MahasiswaDemo04.java
+++ P6/MahasiswaDemo04.java
@@ -44,6 +44,10 @@ public class MahasiswaDemo04 {
         System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
         list.selectionSort();
         list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+        list.insertionSort();
+        list.tampil();
     }
 }
 
