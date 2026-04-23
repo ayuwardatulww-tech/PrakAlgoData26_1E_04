ce0033de12ccbe6f2ad718f7bd39c3a1bd821c70
Ayu Wardatul <ayuwardatuwwl@gmail.com>
2026-04-23 08:30:14 +0700

    2.1 Percobaan 1

---
+++ P9/MAHASISWA04.java
@@ -0,0 +1,18 @@
+package P9;
+
+public class MAHASISWA04 {
+    String nama;
+    String nim;
+    String kelas;
+    int nilai;
+
+    MAHASISWA04(String Nama, String Nim, String kelas){
+        this.nama = Nama;
+        this.nim = Nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+    void tugasDinilia(int nilai){
+        this.nilai = nilai;
+    }
+}
---
+++ P9/stackTugasMahasiswa04.java
@@ -0,0 +1,58 @@
+package P9;
+
+public class stackTugasMahasiswa04 {
+    MAHASISWA04 [] stack;
+    int top;
+    int size;
+    public stackTugasMahasiswa04(int size){
+        this.size = size;
+        stack = new MAHASISWA04[size];
+        top = -1;
+    }
+    public boolean isFull(){
+        if (top == size -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean isEmpty(){
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void push(MAHASISWA04 mhs){
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambah tugas lagi");
+        }
+    }
+    public MAHASISWA04 pop(){
+        if (!isEmpty()) {
+            MAHASISWA04 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai");
+            return null;
+        }
+    }
+    public MAHASISWA04 peek(){
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+    public void print(){
+        for (int i = 0; i <= top; i++) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+}
