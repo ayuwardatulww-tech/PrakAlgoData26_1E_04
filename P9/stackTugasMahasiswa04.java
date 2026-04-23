00f5ffed9136c90c0ff995bb8033e7566e8d4ad9
Ayu Wardatul <ayuwardatuwwl@gmail.com>
2026-04-23 09:01:52 +0700

    2.1 Percobaan 1 Pertanyaan N0.1

--- P9/stackTugasMahasiswa04.java
+++ P9/stackTugasMahasiswa04.java
@@ -50,7 +50,7 @@ public class stackTugasMahasiswa04 {
         }
     }
     public void print(){
-        for (int i = 0; i <= top; i++) {
+        for (int i = top; i >= 0; i--) {
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
         }
         System.out.println("");
