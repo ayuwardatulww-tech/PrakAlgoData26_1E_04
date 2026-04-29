14f7538c5d02370469a97ef9553edfa824a9d949
Ayu Wardatul <ayuwardatuwwl@gmail.com>
2026-04-29 20:36:58 +0700

    Praktikum 2 Modifikasi

--- P10/antriLayanan4.java
+++ P10/antriLayanan4.java
@@ -83,4 +83,13 @@ public class antriLayanan4 {
         }
         
     }
+    public void lihatAkhir(){
+        if (IsEmpty()) {
+            System.out.println("Antrian kosong");
+        } else {
+            System.out.println("Mahasiswa paling belakang: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+    }
 }
