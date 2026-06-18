82fa659c275b80a999f6906291a3a21a26c8993e
Ayu Wardatul <ayuwardatuwwl@gmail.com>
2026-06-18 22:01:07 +0700

    Percobaan 2

---
+++ P16/loopCollection04.java
@@ -0,0 +1,39 @@
+package P16;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class loopCollection04 {
+    public static void main(String[] args) {
+        Stack<String> fruits = new Stack<>();
+        fruits.push("banana");
+        fruits.add("orange");
+        fruits.add("watermelon");
+        fruits.add("leci");
+        fruits.push("salak");
+
+        for (String fruit : fruits) {
+            System.out.printf("%s ", fruit);
+        }
+        System.out.println("\n" + fruits.toString());
+
+        while (!fruits.empty()) {
+            System.out.printf("%s ", fruits.pop());
+        }
+        fruits.push("Melon");
+        fruits.push("durenn");
+        System.out.println("");
+        for (Iterator<String> it = fruits.iterator(); it.hasNext();) {
+            String fruit = it.next();
+            System.out.printf("%s ",fruit);
+        }
+        System.out.println("");
+        fruits.stream().forEach(e -> {
+            System.out.printf("%s ",e);
+        });
+        System.out.println("");
+        for (int i = 0; i < fruits.size(); i++) {
+            System.out.printf("%s ", fruits.get(i));
+        }
+    }
+}
